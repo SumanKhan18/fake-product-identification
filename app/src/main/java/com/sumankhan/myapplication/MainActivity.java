77db11ae5e23e5a804d6b1fef87fd9aa5b3c674c
SumanKhan18 <sumankhan2909@gmail.com>
2024-04-07 12:23:06 +0530

    through blockchain

---
+++ app/src/main/java/com/sumankhan/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sumankhan.myapplication;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+     WebView myApp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.myApp), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        myApp=findViewById(R.id.myApp);
+        myApp.getSettings().setJavaScriptEnabled(true);
+        myApp.setWebViewClient(new WebViewClient());
+        myApp.loadUrl("http://192.168.151.14:3000");
+    }
+}
